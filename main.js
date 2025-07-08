a3f1377de707cc7d62aac4b4ab9eeac180f482ae
Noel-Backhaus <114349123+Noel-Backhaus@users.noreply.github.com>
2025-07-08 15:18:06 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,12 +6,12 @@ function getRndInteger(min, max) {
             
 // Kategorien / Übungen
 
-const arme = ["Bizeps Curls", "Hammer Curls", "Curls weit", "Liegestützen eng", "Rumpfheben"],
-beine = ["Kniebeugen", "Sprint", "Deep walk"],
-brust = ["Liegestütze weit", "Liegestütze gerade", "Liegestütze oben", "Curls eng", "Bankdrücken"],
+const arme = ["Bizeps Curls", "Hammer Curls", "Curls weit", "Liegestützen eng", "Rumpfheben", "Bizeps Curls ohne Hanteln"],
+beine = ["Kniebeugen", "Sprint", "Deep walk", "Waden Jumps"],
+brust = ["Liegestütze weit", "Liegestütze gerade", "Liegestütze oben", "Curls eng", "Bankdrücken", "Side-to-Side-Push-Ups"],
 ruecken = ["Klimmzüge", "Rudern (im Stehen / Sitzen)", "Handtuch zerreißen", "Beugen + Strecken (mit Hanteln)", "Hanteln nach hinten heben", "Pull-overs"],
 schultern = ["Flies", "Hanteln seitlich nach oben", "Liegestütze unten", "Hanteln stemmen (im Stehen)"],
-bauch = ["Sit-Ups", "Cycling", "45° Hold", "Butterflies", "Kerze (Beine hoch und runter heben)", "Seitenstemmen", "Superman"];
+bauch = ["Sit-Ups", "Cycling", "45° Hold", "Butterflies", "Kerze (Beine hoch und runter heben)", "Seitenstemmen", "Superman", "Schienbeine berühren", "Plank Crunches", "Plank Hold", "Plank Jumps", "Einrollen", "Flatter Kicks"];
 
 const koerper = [arme, beine, brust, ruecken, schultern, bauch],
 kategorien = ["Arme", "Beine", "Brust", "Ruecken", "Schultern", "Bauch"];
